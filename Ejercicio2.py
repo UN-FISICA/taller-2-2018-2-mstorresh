1131496665816557fb814957b502971faec28d49
mstorresh <mstorresh@unal.edu.co>
2018-09-08 15:04:20 -0500

    ya finalizado, revisar luego, pero ya funciona

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from turtle import *
-colormode(1)
-pensize(5)
-hideturtle()
-#a=int(input("numero de lados: "))
-a=4
+T1=Turtle()
+T2=Turtle()
+T2.pensize(5)
+T1.hideturtle()
+T2.hideturtle()
+a=int(input("numero de lados: "))
+D1=100  #dimension del cuadrado grande
+d2=20   #dimension de las figuras en los bordes
+d3=d2/2
 for j in range(a):
     if a==2+j:
         y=-180+(a-2)*180/a            
 def figura(x):
     for i in range(a):
-        fd(x)
-        rt(y)
+        T2.fd(x)
+        T2.rt(y)
 def cuadro(z):
     for i in range(4):
-        fd(z)
-        b=pos()
-        c=b-(10,10)
-        figura(20)
-        rt(-90)        
-cuadro(100)
-print (a)
-print (y)
+        T1.penup()
+        T1.fd(z)
+        T2.pendown()
+        b=T1.pos()
+        c=b-(d3,d3)
+        T2.penup()
+        T2.goto(c)
+        T2.pendown()
+        figura(d2)
+        T1.rt(-90)        
+cuadro(D1)
