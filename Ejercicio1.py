58cd4ae6eeb2e434b4db2c24a177ac163c8297c5
mstorresh <mstorresh@unal.edu.co>
2018-09-08 15:01:11 -0500

    ya arreglado, mirar si luego darle algo de estetica, pero ya esta funcional

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
 from turtle import *
-colormode(1)
-pensize(5)
-hideturtle()
+T1=Turtle()
+T2=Turtle()
+T2.pensize(5)
+T1.hideturtle()
+T2.hideturtle()
+D1=100  #dimension del cuadrado grande
+d2=20   #dimension de las figuras en los bordes
+d3=d2/2
 def cuadro(x):
     for i in range(4):
-        fd(x)
-        rt(90)
-penup()
-goto(150,200)
-pendown()
-cuadro(100)
-penup()
-sety(-150)
-pendown()
-cuadro(100)
-penup()
-setx(-200)
-pendown()
-cuadro(100)
-penup()
-sety(200)
-pendown()
-cuadro(100)
+        T2.fd(x)
+        T2.rt(-90)
+def cuadrado(z):
+    for i in range(4):
+        T1.penup()
+        T1.fd(z)
+        T2.pendown()
+        b=T1.pos()
+        c=b-(d3,d3)
+        T2.penup()
+        T2.goto(c)
+        T2.pendown()
+        cuadro(d2)
+        T1.rt(-90) 
+cuadrado(100)
