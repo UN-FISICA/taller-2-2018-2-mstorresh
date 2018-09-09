1c77e4afcb29c3d8a9b66f30173399e454a342fc
mstorresh <mstorresh@unal.edu.co>
2018-09-09 15:36:13 -0500

    cuando los lados son 6 o mas, la figura crece mucho, toca mirar como ajustar eso

---
+++ Ejercicio4.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from turtle import *
+import math as m
+b=int(input("numero de lados de las figuras en los bordes : "))
+a=int(input("numero de filas de la piramide: "))
+for l in range(b): 
+    if b==2+l:
+        y=180-(b-2)*180/b 
+def figura(x):
+    for i in range(b):
+        fd(x)
+        rt(y)        
+d=0
+for j in range(a,0,-1):
+    c=0
+    print(j)
+    while c<j:
+        penup()
+        seth(90-180/a)
+        fd(25/m.sqrt(1/a)+20)
+        seth(0)
+        pendown()
+        figura(50)
+        penup()
+        right(90+180/a)
+        fd(25/m.sqrt(1/a)+20)
+        seth(0)
+        fd(88)
+        c += 1    
+    setposition((d+1)*40,((d+1)*100))       
+    d +=  1
+exitonclick()
