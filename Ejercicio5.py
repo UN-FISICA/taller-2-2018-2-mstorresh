ebc2909f9b715ec7d0a18754d027866751978bf3
mstorresh <mstorresh@unal.edu.co>
2018-09-09 17:05:26 -0500

    pasa lo mismo con los anteriores, al ser n grande no queda tan bien

---
+++ Ejercicio5.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from turtle import *
+import math as m
+a=int(input("numero de filas de la piramide: "))
+#hideturtle()
+
+def figura(x):
+    for l in range(a+2,2,-1):
+        if l==j+2:
+            for i in range(l):
+                y=-180+(l-2)*180/l
+                fd(x)
+                rt(y)
+                print(y)
+d=0
+for j in range(a,0,-1):
+    c=0
+    while c<j:
+        #penup()
+        seth(90-180/a)
+        fd(25/m.sqrt(1/a)+20)
+        seth(0)
+        #pendown()
+        figura(50)
+        #penup()
+        right(90+180/a)
+        fd(25/m.sqrt(1/a)+20)
+        seth(0)
+        fd(88)
+        c += 1    
+    setposition((d+1)*40,((d+1)*100))       
+    d +=  1
+exitonclick()
